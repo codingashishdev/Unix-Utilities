c8d08992a0d520eff60ddb799303f5d0a3ac0700
codingashishdev <atankmember@gmail.com>
2025-06-06 11:29:52 +0530

    Implement grep utility to search and highlight target words in a file

--- grep.go
+++ grep.go
@@ -3,8 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
-func grep(){
-	
+func main() {
+	grep()
+}
+
+func grep() {
+
+	if len(os.Args[1]) < 2 {
+		fmt.Println("Something went while reading the file")
+		return
+	}
+
+	fileName := os.Args[1]
+	target := os.Args[2]
+
+	file, err := os.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println("Something went while reading the content of file")
+	}
+
+	content := string(file)
+
+	words := strings.Split(content, " ")
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == target {
+			highlighted := strings.Replace(content, words[i], "\033[31m"+words[i]+"\033[0m", i)
+			fmt.Println(highlighted)
+		}
+	}
 }
